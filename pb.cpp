b9d70a28e359166f2d3aa5b1980a886b8188203a
attack-on-shuvon <shuvonjr97@gmail.com>
2025-07-28 21:38:49 +0600

    Add files via upload

---
+++ pb.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num = 153;
+    int digit = 3;
+
+    int result = pow(1,3)+pow(5,3)+pow(3,3);
+
+    if(result == num){
+        cout<<"amstrong";
+    }
+
+
+
+
+}
